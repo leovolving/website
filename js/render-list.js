e5065985cf90cec483b1c67132539f046097c543
Leo Yockey <leoyockey@gmail.com>
2022-01-12 16:54:07 -0800

    make js reusable

---
+++ js/render-list.js
@@ -0,0 +1,42 @@
+const createElement = (tag, classNames = "") => {
+  const element = document.createElement(tag);
+  element.className = classNames;
+  return element;
+};
+
+const renderList = (data, type) => {
+  const ul = createElement("ul", `list ${type}-list`);
+
+  for (let i = 0; i < data.length; i++) {
+    const item = data[i];
+
+    const a = createElement("a");
+    a.innerText = item.title;
+    a.href = item.href;
+
+    const li = createElement("li");
+    li.appendChild(a);
+
+    if (item.date) {
+      const span = createElement("span");
+      span.innerText = new Date(item.date).toLocaleDateString();
+      li.appendChild(span);
+
+      const p = createElement("p");
+      p.innerText = item.tags.join(" | ");
+      li.appendChild(p);
+    }
+
+    ul.appendChild(li);
+  }
+
+  const container = document.getElementById(`${type}-list-container`);
+  container.appendChild(ul);
+};
+
+const fetchList = (type) => {
+  fetch(`/js/${type}.json`)
+    .then((response) => response.json())
+    .then((data) => renderList(data, type))
+    .catch((error) => console.log(error));
+};
